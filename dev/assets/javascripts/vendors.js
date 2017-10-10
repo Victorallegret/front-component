5212c747fe28f9cb1a6d960187465565820b87f8
Victor Allegret <victor.allegret@gmail.com>
2017-10-10 15:18:17 +0200

    FIX [bootstrap popper] Add popper to fix bootstra pissue

--- dev/assets/javascripts/vendors.js
+++ dev/assets/javascripts/vendors.js
@@ -1,4 +1,6 @@
 window.jQuery = window.$ = require('jquery');
 require('./vendors/turbolinks.js');
+window.Popper = require('popper.js')
+require('bootstrap');
 require("waypoints");
 require("slick-carousel");
